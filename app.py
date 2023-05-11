b37a788037ef4516799b4aeb47fd622d61472dbb
DIEGO RAGOZINI SILVA <diego.rsilva52@senacsp.edu.br>
2023-05-11 11:48:34 -0300

    Aula 11/05/23

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world!'
+    return render_template('index.html')
+
+@app.route('/sobre_ead')
+def sobre_ead():
+    return render_template('sobre_ead.html')
+
+@app.route('/cursos')
+def cursos():
+    return render_template('cursos.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
 
 if __name__ == '__main__':
