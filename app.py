03526fb13f5a728ce668127dd405ca6ff110d9ab
DIEGO RAGOZINI SILVA <diego.rsilva52@senacsp.edu.br>
2023-06-01 12:04:57 -0300

    aula 10623

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, url_for
+import mysql.connector
+from flask import Flask, render_template, request, url_for, flash, redirect
 from forms import formLogin, formNovoUsuario
+from hashlib import sha256
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '07ced41fe132b166347a6f014a704f51aacfddf919ad41b182562d0668b344f8'
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'P@$$w0rd',
+    database = 'ead_senac'
+)
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,6 +32,18 @@ def login():
 
     form_login = formLogin()
     form_novo_usuario = formNovoUsuario()
+    
+    if form_login.validate_on_submit() and 'submitLogin' in request.form:
+
+        flash(f'Login realizado com sucesso: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('index'))
+
+    if form_novo_usuario.validate_on_submit() and 'submitCadastro' in request.form:
+    
+        cursor = mydb.cursor()
+    
+
+
 
 
     return render_template('login.html', titulo=titulo, 
